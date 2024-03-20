86525a9df69f6a9fb1df3672999e5aeac35898ef
Tapio Suominen <suominen.tapio@outlook.com>
2024-03-20 13:17:17 +0200

    T1

---
+++ T1-10/src/Main/java/com/example/T2.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class T2
+{
+    public static void main( String[] args )
+    {
+
+
+        System.out.println("Hello world");
+    }
+}
--- T1-10/src/main/java/com/example/Main.java
+++ T1-10/src/main/java/com/example/Main.java
@@ -4,6 +4,10 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        double x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+
+        double distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+
+        System.out.println("Kahden pisteen välinen etäisyys on: " + distance);
     }
 }
