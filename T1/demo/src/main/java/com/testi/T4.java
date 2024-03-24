fcaa79208bbedba4f23e5c113306119030b454fc
Tapio Suominen <125696453+Suomin3@users.noreply.github.com>
2024-03-24 15:12:25 +0200

    T1-6

---
+++ T1/demo/src/main/java/com/testi/T4.java
@@ -0,0 +1,35 @@
+package com.testi;
+
+public class T4
+{
+    public static void main(String[] args)
+    {
+        int[][] matrix = {
+            {1, 5, 7},
+            {9, 3, 1},
+            {0, 1, 1}
+        };
+        int[] maxIndex = findMaxIndex(matrix);
+
+        System.out.println("Vastaus: [" + maxIndex[0] + ", " + maxIndex[1] + "]");
+    }
+    public static int[] findMaxIndex(int[][] matrix)
+    {
+        int max = Integer.MIN_VALUE;
+        int[] maxIndex = new int[2];
+        
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[i].length; j++)
+            {
+                if (matrix[i][j] > max)
+                {
+                    max = matrix[i][j];
+                    maxIndex[0] = i;
+                    maxIndex[1] = j;
+                }
+            }
+        }
+        return maxIndex;
+    }
+}
